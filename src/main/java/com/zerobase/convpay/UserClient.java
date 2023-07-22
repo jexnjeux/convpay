57e0fa4288f721a552a08a4ef15aca17990c1a5d
jexnjeux <zieun.na@gmail.com>
2023-07-22 17:47:37 +0900

    xml을 통한 빈 등록

--- src/main/java/com/zerobase/convpay/UserClient.java
+++ src/main/java/com/zerobase/convpay/UserClient.java
@@ -1,6 +1,6 @@
 package com.zerobase.convpay;
 
-import com.zerobase.convpay.config.ApplicationConfig;
+//import com.zerobase.convpay.config.ApplicationConfig;
 import com.zerobase.convpay.dto.PayCancelRequest;
 import com.zerobase.convpay.dto.PayCancelResponse;
 import com.zerobase.convpay.dto.PayRequest;
@@ -10,14 +10,15 @@ import com.zerobase.convpay.type.ConvenienceType;
 import com.zerobase.convpay.type.PayMethodType;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class UserClient {
 
   public static void main(String[] args) {
 //     '사용자' -> 편결이 -> 머니
 
-    ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-        ApplicationConfig.class);
+    ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+        "spring-config.xml");
 
     ConveniencePayService conveniencePayService = applicationContext.getBean(
         "conveniencePayService", ConveniencePayService.class);
